9b34c763def5575a41ebc2c2c8ab0f549fdce7d7
ckorthof <ckorthof@gmail.com>
2017-12-23 21:32:52 +0100

    Adde an extra line.

--- aapje3.py
+++ aapje3.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
+# aapje3.py
+
+
 
 #This is a dictionary
 
 aap_dict = {'bonobo': 'black', 'Chimpanzee': 'brown', 'greyback': 'grey'}
 
 print("A bonobo is: " + aap_dict['bonobo'])
+print("A greyback is: " + aap_dict['greyback'])
