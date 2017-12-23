d91965f2da024be9e13a7bc3dcfef10ae07b6bee
ckorthof <ckorthof@gmail.com>
2017-12-23 21:53:49 +0100

    Another meaningless updatre... Just testing, you see??

--- aapje2.py
+++ aapje2.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
 #This is a list
-aap_list = [12, 34, 56, 78 , 90]
+aap_list = [12, 34, 56, 78 , 90, 451]
 sum_list = sum(aap_list)
 len_list = len(aap_list)
 result = sum_list/len_list
-print("Het resultaat is: " + str(result) )
+print("Het gemiddelde is: " + str(result) )
+print("Het aantale elementen in de lijst is: " + str(len_list))
+print("De elementen opgeteld geeft: " + str(sum_list))
