aaf860ddcdc31233fa72353a3e243c615c1be6a5
ckorthof <ckorthof@gmail.com>
2017-12-23 21:22:59 +0100

    The first 'aapje' application.

---
+++ aapje2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+#This is a list
+aap_list = [12, 34, 56, 78 , 90]
+sum_list = sum(aap_list)
+len_list = len(aap_list)
+result = sum_list/len_list
+print("Het resultaat is: " + str(result) )
---
+++ aapje3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+#This is a dictionary
+
+aap_dict = {'bonobo': 'black', 'Chimpanzee': 'brown', 'greyback': 'grey'}
+
+print("A bonobo is: " + aap_dict['bonobo'])
