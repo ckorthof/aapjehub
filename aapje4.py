0c94a9f7284b0c382b0115d2d0bf45685481a6ae
ckorthof <ckorthof@gmail.com>
2017-12-29 22:03:14 +0100

    Even more variables in aapje4.py

--- aapje4.py
+++ aapje4.py
@@ -3,11 +3,14 @@
 var = "Stringetje "
 var2 = 3.75
 var3 = 3,75
+var4 = {"Piet": "Jansen", 23: 14, "Score": 3}
 
 var *= 3
 var2 *= 3
 var3 *= 3
+#var4 *= 3
 
 print(var)
 print(var2)
 print(var3)
+print(var4)
