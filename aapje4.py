d5163eae32a63cd1d5f769b6877fe661df5a0c1d
ckorthof <ckorthof@gmail.com>
2017-12-31 15:05:05 +0100

    aapje4.py now contains a simple loop though dictionary keys.

--- aapje4.py
+++ aapje4.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+from all_passwords import password
+
+passworddict = password()
+
 var = "Stringetje "
 var2 = 3.75
 var3 = 3,75
@@ -15,3 +19,6 @@ print(var2)
 print(var3)
 print(var4)
 print("De score is: " + str(var4['Score']))
+for userid in passworddict.keys():
+    pwd = passworddict[userid]
+    print("Password van " + userid + ": " + pwd)
